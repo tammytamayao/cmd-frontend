6fb30f3c39a97bb7aeaf28f10556df75601785e0
tammytamayao <tammytamayao@gmail.com>
2025-11-14 12:02:22 +0800

    fix: login page width

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import LoginPage from "./login/page";
 
 export default function Home() {
   return (
-    <div className="min-h-screen grid place-items-center bg-gray-50 px-4">
-      <main className="min-h-screen grid place-items-center px-4">
+    <div>
+      <main>
         <LoginPage />
       </main>
     </div>
